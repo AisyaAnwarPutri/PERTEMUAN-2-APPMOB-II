776d96543a33154c8a62c3f843b28b6d3289a8b4
Aisya <aisyanwarputri@gmail.com>
2022-03-18 13:55:24 +0700

    30_Safe_Calls

---
+++ src/main/kotlin/30_Safe_Calls.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //Safe calling
+    // ?.let
+    var maybeNumber: Int? = 15
+    maybeNumber?.let { println(it) }
+
+//Itu ?. rantai
+//mahasiswa?.guru?.supervisor?.name = "Seymour Skinner"
+}
